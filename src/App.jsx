c4ba7e3355761c90e8d0df103f13b807be24d440
samsuni1999 <samsunil099@gmail.com>
2024-07-20 10:01:27 +0530

    select file from uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 
 const App = () => {
-  const [files, setFiles] = useState([]);
+  const [file, setFile] = useState([]);
+  const [uploadedFiles, setUploadedFiles] = useState([])
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const [outputMessages, setOutputMessages] = useState([])
   const [disableSentBtn, setDisableSentBtn] = useState(false)
-  const handleFileChange = (e) => {
-    const selectedFiles = Array.from(e.target.files);
-    if (selectedFiles.length == 0) {
-      return
-    }
-    const updatedFiles = selectedFiles[0]
-    setFiles([...files, updatedFiles]);
-  };
+  const [selectedFile, setSelectedFile] = useState(null);
 
-  const handleSendMessage = (e) => {
+  const fileInputRef = useRef(null);
+
+  const handleFileChange = (e) => {
+    const chosenFile = Array.from(e.target.files);
+    setFile(chosenFile);
+  };
+  
+  const handleSendMessage = () => {
     if (message.trim()) {
       setDisableSentBtn(true)
       setMessages([...messages, message]);
       setTimeout(() => {
         fetch("./data/data.json")
-        .then((res) => {
-          return res.json()
-        })
-        .then(data => {
-          // setChatOutput(data.response)
-          // console.log(data.response)
-          setOutputMessages([...outputMessages, data.response])
-        }
-      )
-      .catch((err) => {
-        console.log(err)
+          .then((res) => {
+            return res.json()
+          })
+          .then(data => {
+            setOutputMessages([...outputMessages, data.response])
+          }
+          )
+          .catch((err) => {
+            console.log(err)
+          })
+
+        setDisableSentBtn(false)
+        setMessage('');
+
       })
-      
-      setDisableSentBtn(false)
-      setMessage('');
-      
-      }, 2000)
     }
+  };
+
+  const handleUploadFile = () => {
+    if (file.length == 0) {
+      return
+    }
+
+    for (let f of uploadedFiles) {
+      if (f.name === file[0].name) {
+        alert(`File ${f.name} is already uploaded`);
+        return
+      }
+    }
+
+    setFile([]);
+    setUploadedFiles([...uploadedFiles, file[0]])
+    if (fileInputRef.current) {
+      fileInputRef.current.value = '';
+    }
+  };
+
+  const handleFileSelect = (file) => {
+    setSelectedFile(file);
   };
 
   return (
     <div className="container">
       <div className="left">
         <h2>Upload Files</h2>
-        <input type="file" multiple onChange={handleFileChange} />
+        <input
+          type="file"
+          id="choose-file-input"
+          onChange={handleFileChange}
+          ref={fileInputRef}
+          accept=".pdf, .doc, .csv, .xlsx, .xls"
+        />
+        <button onClick={handleUploadFile}>Upload</button>
         <div className="file-list">
-          <h3>Uploaded Files:</h3>
-          <ul>
-            {files.map((file) => (
-
-              <li key={file.name}>{file.name}</li>
+          <h2>Uploaded Files:</h2>
+          {uploadedFiles.length > 0 && <div className="files">
+            {uploadedFiles.map((f) => (
+              <div 
+              className={`uploaded-file ${selectedFile && selectedFile.name === f.name ? 'selected' : ''}`} 
+              key={f.name}
+              onClick={() => handleFileSelect(f)}
+              >
+                <p>{f.name}</p>
+              </div>
             ))}
-          </ul>
+          </div>}
+
         </div>
-        <button onClick={handleSendMessage}>Upload</button>
+        <div className=""></div>
       </div>
       <div className="right">
         <h2>Chat</h2>
@@ -75,9 +110,9 @@ const App = () => {
             type="text"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            placeholder="Type your message..."
+            placeholder="Select an uploaded file and type your message..."
           />
-          <button disabled={disableSentBtn} onClick={handleSendMessage}>Send</button>
+          <button disabled={!disableSentBtn && selectedFile === null} onClick={handleSendMessage}>Send</button>
         </div>
       </div>
     </div>
